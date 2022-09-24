33aceaccfe5bc00809351a4f3d7c86782dd03e12
marina-t-itf <toader.marinaloredana@gmail.com>
2022-09-24 13:27:00 +0300

    create bootstrap nav-bar and create map

---
+++ main.js
@@ -0,0 +1,28 @@
+// crearea unei harti in JS
+let map = L
+    .map('map')
+    .setView(
+        [44.42, 26.10],
+         10
+    )
+;
+
+L
+    .tileLayer(
+        'https://tile.openstreetmap.org/{z}/{x}/{y}.png', 
+        {
+            maxZoom: 12,
+            minZoom: 3,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }
+    )
+    .addTo(map)
+;
+
+// creare marcator in functie de coordonate
+let marker = L
+    .marker(
+        [44.42, 26.10]
+    )
+    .addTo(map)
+;
