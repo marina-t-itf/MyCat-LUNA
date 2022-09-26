9369f8f9d5229ff5796549d2670dc588982ba86d
marina-t-itf <toader.marinaloredana@gmail.com>
2022-09-26 15:41:47 +0300

    Create popup galery

---
+++ galery/secondPage.js
@@ -0,0 +1,16 @@
+let popupCloseButton = document.querySelector('.closeButton>span');
+let showPopup = document.querySelector('.galery');
+
+let popupClose = function() {
+    let popup = document.querySelector('.popup');
+    popup.style.display = 'none';
+};
+
+popupCloseButton.addEventListener('click', popupClose);
+
+let popupOpen = function() {
+    let popup = document.querySelector('.popup');
+    popup.style.display = 'block';
+}
+
+showPopup.addEventListener('click', popupOpen);
