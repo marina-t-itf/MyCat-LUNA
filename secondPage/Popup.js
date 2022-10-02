95aef86965fb11d5d896762d9d9a0fbf0a5c4b2f
marina-t-itf <toader.marinaloredana@gmail.com>
2022-10-02 16:15:01 +0300

    create a carousel gallery

--- secondPage/Popup.js
+++ secondPage/Popup.js
@@ -1,5 +1,9 @@
+
 let popupCloseButton = document.querySelector('.closeButton>span');
 let showPopup = document.querySelector('.fotoGalley');
+let popupBody = document.querySelector('.popup');
+let counterNext = 0;
+// let counterPrev = 4;
 
 let popupClose = function() {
     let popup = document.querySelector('.popup');
@@ -9,9 +13,34 @@ let popupClose = function() {
 popupCloseButton.addEventListener('click', popupClose);
 
 let popupOpen = function() {
+    
     let popup = document.querySelector('.popup');
     popup.style.display = 'block';
+    // for (let i=0; i<6; i++) {
+        popupBody.style.backgroundImage = `url(./3rd-party/image/foto1.JPG)`;
+        // popupBody.style.backgroundImage = `url(fotoGallery[i])`;
+        popupBody.style.backgroundSize = "cover";
+        popupBody.style.backgroundPosition = "center center";
+
+    // }
 }
 
 showPopup.addEventListener('click', popupOpen);
 
+
+let onNextClick = function() {
+    let next = document.querySelector('.popup');
+    if( counterNext < 5) {
+        popupBody.style.backgroundImage = 'url(' +'./3rd-party/image/foto' + (counterNext+1) +'.JPG' + ')';
+        counterNext++;
+    }
+}
+
+let onPreviewClick = function() {
+    let prev = document.querySelector('.popup');
+        if(counterNext > 1) {
+            popupBody.style.backgroundImage = 'url(' +'./3rd-party/image/foto' + (counterNext-1) +'.JPG' + ')';
+            counterNext--;
+        }
+
+}
--- secondPage/chart.js
+++ secondPage/chart.js
@@ -1,11 +1,11 @@
-let xValues = ["somn", "joaca", "doresc plic", "imi trezesc stapanii", "sunt cuminte"];
+let xValues = ["dorm", "ma joc", "doresc plic", "imi trezesc stapanii", "sunt cuminte"];
 let yValues = [50, 20, 15, 12, 3];
 let barColors = [
-  "#2a6e28",
-  "#eeb414",
-  "#209e1b",
+  "rgb(9, 61, 121)",
+  "rgb(243, 89, 0)",
+  "rgb(9, 121, 116)",
   "#ce8600",
-  "#0a4d1a"
+  "rgb(35, 121, 9)"
 ];
 
 new Chart("myChart", {
